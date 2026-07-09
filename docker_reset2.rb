3b45482c87de74586adcbc0075bb8ff37aebcd7b
Mario <109123293+darkmariod@users.noreply.github.com>
2026-07-09 14:21:43 -0500

    Limpieza: quitar scripts de prueba y archivos temporales

--- docker_reset2.rb
+++
@@ -1,5 +0,0 @@
-u = User.find_by(email: "demo@finanzas.com")
-u.subscriptions.where(auto_create: false).destroy_all
-u.subscriptions.update_all(last_created_at: nil)
-u.transactions.where(description: u.subscriptions.pluck(:name)).destroy_all
-puts "Cleaned. Users subs: #{u.subscriptions.count}"
